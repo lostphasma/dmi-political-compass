b3a9832c30518205d0c1ffdf07201dfeb7b2ab35
EleCapp <eleonora.cappuccio@outlook.it>
2019-07-10 17:05:43 +0200

    description_tribes

--- js/data.js
+++ js/data.js
@@ -1,46 +1,572 @@
-var geometries = [{
-    r: 1,
-    x: 0,
-    y: 3,
-    z: -50,
-    url: "https://www.youtube.com",
-    title: "Title 1 here",
-    content: "Lorem sit amet 1 This will be the description",
-    imageName: "image-name-1.jpg"
-},{
-    r: 1,
-    x: 3,
-    y: 10,
-    z: -20,
-    url: "https://www.youtube.com",
-    title: "Title 2 here",
-    content: "Lorem sit amet 2 This will be the description",
-    imageName: "image-name-2.jpg"
-},{
-    r: 1,
-    x: -3,
-    y: 0,
-    z: 15,
-    url: "https://www.youtube.com",
-    title: "Title 3 here",
-    content: "Lorem sit amet 3 This will be the description",
-    imageName: "image-name-3.jpg"
-},{
-    r: 1,
-    x: -20,
-    y: 0,
-    z: 5,
-    url: "https://www.youtube.com",
-    title: "Title 4 here",
-    content: "Lorem sit amet 4 This will be the description",
-    imageName: "image-name-4.jpg"
-},{
-    r: 1,
-    x: 20,
-    y: 4,
-    z: 12,
-    url: "https://www.youtube.com",
-    title: "Title 5 here",
-    content: "Lorem sit amet 5 This will be the description",
-    imageName: "image-name-5.jpg"
-}] 
+var geometries = [
+    {
+       r: 1,
+       x: 75,
+       y: 40,
+       z: -25,
+       url: "",
+       title: "SJWs (Social Justice Warriors)",
+       content: "Trigger warning please",
+       imageName: "SJWs.png"
+       },
+    {
+       r: 1,
+       x: 60,
+       y: 50,
+       z: -10,
+       url: "",
+       title: "Animal Rights Activists",
+       content: "Animal Rights = Human Rights",
+       imageName: "Animal Rights Activists.jpg"
+       },
+    {
+       r: 1,
+       x: 67,
+       y: -20,
+       z: -25,
+       url: "",
+       title: "Black Lives Matter",
+       content: "Hand's Up, Don't Shoot ",
+       imageName: "Black Lives Matter.png"
+       },
+    {
+       r: 1,
+       x: 80,
+       y: 30,
+       z: -5,
+       url: "",
+       title: "#MeToo",
+       content: "Time's Up",
+       imageName: "MeToo.jpg"
+       },
+    {
+       r: 1,
+       x: 40,
+       y: 40,
+       z: -35,
+       url: "",
+       title: "4th Wave Feminist",
+       content: "No feminism without intersectionalism ",
+       imageName: "4th Wave Feminist.jpg"
+       },
+    {
+       r: 1,
+       x: 5,
+       y: -30,
+       z: -50,
+       url: "",
+       title: "PoC Radical Feminist  ",
+       content: "Angela Davis",
+       imageName: "PoC Radical Feminist.jpg"
+       },
+    {
+       r: 1,
+       x: 75,
+       y: 5,
+       z: -15,
+       url: "",
+       title: "LGBT",
+       content: "All love is equal",
+       imageName: "LGBT.png"
+       },
+    {
+       r: 1,
+       x: 30,
+       y: 10,
+       z: -30,
+       url: "",
+       title: "Gender Deconstructionists",
+       content: "Gender is a social construct",
+       imageName: "Gender Deconstructionists.png"
+       },
+    {
+       r: 1,
+       x: 30,
+       y: 60,
+       z: -10,
+       url: "",
+       title: "Gender-Critical Feminists",
+       content: "Gender is not a social construct",
+       imageName: "Gender-Critical Feminists.png"
+       },
+    {
+       r: 1,
+       x: -40,
+       y: -75,
+       z: -55,
+       url: "",
+       title: "Queer anarchists",
+       content: "Gender is state oppression",
+       imageName: "Queer anarchists.png"
+       },
+    {
+       r: 1,
+       x: 50,
+       y: -50,
+       z: -80,
+       url: "",
+       title: "Neo-Marxists",
+       content: "Workers of the world, re-unite!",
+       imageName: "Neo-Marxists.png"
+       },
+    {
+       r: 1,
+       x: 75,
+       y: 0,
+       z: -55,
+       url: "",
+       title: "Antifa",
+       content: "NAZIS RAUS",
+       imageName: "Antifa.jpg"
+       },
+    {
+       r: 1,
+       x: 50,
+       y: -15,
+       z: -20,
+       url: "",
+       title: "Occupy",
+       content: "We are the 99%",
+       imageName: "Occupy.jpg"
+       },
+    {
+       r: 1,
+       x: -20,
+       y: 45,
+       z: -45,
+       url: "",
+       title: "Dirtbag Left",
+       content: ""That's praxis" (alternate: posting is praxis)",
+       imageName: "Dirtbag Left.jpg"
+       },
+    {
+       r: 1,
+       x: 20,
+       y: 45,
+       z: -18,
+       url: "",
+       title: "DSA (Democratic Socialists of America)",
+       content: "Bernie would have won ",
+       imageName: "DSA.jpg"
+       },
+    {
+       r: 1,
+       x: 90,
+       y: 75,
+       z: -2,
+       url: "",
+       title: "Establishment Left",
+       content: "Still with her.",
+       imageName: "Establishment Left.jpg"
+       },
+    {
+       r: 1,
+       x: -70,
+       y: 90,
+       z: -65,
+       url: "",
+       title: "Tankies",
+       content: "Stalin did nothing wrong",
+       imageName: "Tankies.png"
+       },
+    {
+       r: 1,
+       x: 95,
+       y: 70,
+       z: 10,
+       url: "",
+       title: "Neo-Panglossians",
+       content: "I've got a good feeling about this!",
+       imageName: "Neo-Panglossians.jpg"
+       },
+    {
+       r: 1,
+       x: 0,
+       y: -5,
+       z: 40,
+       title: "New Atheists",
+       url: "",
+       content: "In this moment, I am euphoric. Not because of any phony god's blessing. But because, I am enlightened by my intelligence",
+       imageName: "New Atheists.jpg"
+       },
+    {
+       r: 1,
+       x: -35,
+       y: 0,
+       z: 30,
+       url: "",
+       title: "Street Epistemologists",
+       content: "Socrates.",
+       imageName: "Street Epistemologists.jpg"
+       },
+    {
+       r: 1,
+       x: -15,
+       y: 15,
+       z: 20,
+       url: "",
+       title: "Rationalist Diaspora",
+       content: "What would Bayes do?",
+       imageName: "Rationalist Diaspora.png"
+       },
+    {
+       r: 1,
+       x: -30,
+       y: -50,
+       z: 20,
+       url: "",
+       title: "Integral Theorists",
+       content: "It's all connected, man",
+       imageName: "Integral Theorists.jpg"
+       },
+    {
+       r: 1,
+       x: 25,
+       y: 85,
+       z: 50,
+       url: "",
+       title: "Sorters",
+       content: "🐸 Lobsters are hierarchical!",
+       imageName: "Sorters.jpg"
+       },
+    {
+       r: 1,
+       x: 12,
+       y: 75,
+       z: 45,
+       url: "",
+       title: "Intellectual Dark Web",
+       content: "Debate me",
+       imageName: "Intellectual Dark Web.png"
+       },
+    {
+       r: 1,
+       x: -45,
+       y: 95,
+       z: 65,
+       url: "",
+       title: "Crusaders",
+       content: "Deus vult",
+       imageName: "Crusaders.png"
+       },
+    {
+       r: 1,
+       x: -20,
+       y: -40,
+       z: -35,
+       url: "",
+       title: "Neo-Paganists",
+       content: "Witchcraft, with more crystals and less brooms ",
+       imageName: "Neo-Paganists.jpg"
+       },
+    {
+       r: 1,
+       x: -5,
+       y: 0,
+       z: 65,
+       url: "",
+       title: "Odinism",
+       content: "I am a white supremacist",
+       imageName: "Odinism.jpg"
+       },
+    {
+       r: 1,
+       x: 95,
+       y: 85,
+       z: 60,
+       url: "",
+       title: "Christian Right",
+       content: "What would Jesus do?",
+       imageName: "Christian Right.jpg"
+       },
+    {
+       r: 1,
+       x: 55,
+       y: -20,
+       z: 85,
+       url: "",
+       title: "Libertarians",
+       content: "Anarcho-capitalism for the 1%",
+       imageName: "Libertarians.png"
+       },
+    {
+       r: 1,
+       x: 87,
+       y: 55,
+       z: 55,
+       url: "",
+       title: "Establishment Right",
+       content: "Please clap",
+       imageName: "Establishment Right.png"
+       },
+    {
+       r: 1,
+       x: 82,
+       y: -5,
+       z: 65,
+       url: "",
+       title: "Tea Party",
+       content: "more like oBUMMER",
+       imageName: "Tea Party.png"
+       },
+    {
+       r: 1,
+       x: -25,
+       y: 75,
+       z: 60,
+       url: "",
+       title: "Trumpists",
+       content: "Make America Great Again",
+       imageName: "Trumpists.jpg"
+       },
+    {
+       r: 1,
+       x: -55,
+       y: -60,
+       z: 75,
+       title: "InfoWarriors",
+       url: "",
+       content: "Everything I don't like is a false flag - alternatively: "they're putting chemicals in the water that turn the friggin' frogs gay."",
+       imageName: "InfoWarriors.png"
+       },
+    {
+       r: 1,
+       x: -65,
+       y: 85,
+       z: 75,
+       url: "",
+       title: "QAnoners",
+       content: "Where we go one we go all ",
+       imageName: "QAnoners.jpg"
+       },
+    {
+       r: 1,
+       x: 45,
+       y: 0,
+       z: 50,
+       url: "",
+       title: "Alt-Lite",
+       content: "Triggered, libtard? ",
+       imageName: "Alt-Lite.jpg"
+       },
+    {
+       r: 1,
+       x: 10,
+       y: 40,
+       z: 70,
+       url: "",
+       title: "Alt-Right",
+       content: "Dress well and hate things",
+       imageName: "Alt-Right.jpg"
+       },
+    {
+       r: 1,
+       x: 90,
+       y: 87,
+       z: 80,
+       url: "",
+       title: "Modern Neo-Nazis",
+       content: "We must secure the existence of our people and a future for white children",
+       imageName: "Modern Neo-Nazis.png"
+       },
+    {
+       r: 1,
+       x: -80,
+       y: 90,
+       z: 83,
+       url: "",
+       title: "Neoreactionaries",
+       content: ""I no longer believe that freedom and democracy are compatible"?",
+       imageName: "Neoreactionaries.jpg"
+       },
+    {
+       r: 1,
+       x: -30,
+       y: 10,
+       z: 50,
+       url: "",
+       title: "Gamergaters",
+       content: "Get your politics out of my video games",
+       imageName: "Gamergaters.jpg"
+       },
+    {
+       r: 1,
+       x: 20,
+       y: 25,
+       z: 25,
+       url: "",
+       title: "MRA (Mens Right Advocates)",
+       content: "Men have rights too! ",
+       imageName: "MRA.jpg"
+       },
+    {
+       r: 1,
+       x: -30,
+       y: -30,
+       z: 35,
+       url: "",
+       title: "The Red Pill",
+       content: "Down the misogynist rabbit hole",
+       imageName: "The Red Pill.png"
+       },
+    {
+       r: 1,
+       x: -10,
+       y: -45,
+       z: 20,
+       url: "",
+       title: "MGTOW (Men Going Their Own Way)",
+       content: "Lesbian separatism, but for men (but no homo)",
+       imageName: "MGTOW.png"
+       },
+    {
+       r: 1,
+       x: -60,
+       y: -40,
+       z: 40,
+       url: "",
+       title: "Incels",
+       content: "Forever alone",
+       imageName: "Incels.png"
+       },
+    {
+       r: 1,
+       x: -75,
+       y: -85,
+       z: -90,
+       url: "",
+       title: "Anarcho-communism",
+       content: "Whats mine is yours",
+       imageName: "Anarcho-communism.png"
+       },
+    {
+       r: 1,
+       x: -90,
+       y: -95,
+       z: 0,
+       url: "",
+       title: "Anarcho-primitivism",
+       content: "Luddism for anarchists",
+       imageName: "Anarcho-primitivism.jpg"
+       },
+    {
+       r: 1,
+       x: -60,
+       y: -70,
+       z: 0,
+       url: "",
+       title: "Anarcho-transhumanism",
+       content: "I want to be a computer (if we were all computers, we'd finally be equal)",
+       imageName: "Anarcho-transhumanism.jpg"
+       },
+    {
+       r: 1,
+       x: -55,
+       y: -85,
+       z: 90,
+       url: "",
+       title: "Anarcho-capitalism",
+       content: ""I was beat up by left anarchists in Greece. (self.Anarcho_Capitalism)"",
+       imageName: "Anarcho-capitalism.jpg"
+       },
+    {
+       r: 1,
+       x: -15,
+       y: -75,
+       z: -70,
+       url: "",
+       title: "Anarcho-syndicalist ",
+       content: "Workers of the world, unite! (but not too much)",
+       imageName: "Anarcho-syndicalist .png"
+       },
+    {
+       r: 1,
+       x: -62,
+       y: -65,
+       z: 83,
+       url: "",
+       title: "Agorists",
+       content: "What if everything was bought and sold like drugs?",
+       imageName: "Agorists.jpg"
+       },
+    {
+       r: 1,
+       x: -42,
+       y: -55,
+       z: -75,
+       url: "",
+       title: "Anarcho-pacifists ",
+       content: "uwu wevowution pwease? (don't hit me)",
+       imageName: "Anarcho-pacifists .jpg"
+       },
+    {
+       r: 1,
+       x: -10,
+       y: -80,
+       z: 5,
+       url: "",
+       title: "Mutualists (also anarcho-mututalists) ",
+       content: "It's free real estate",
+       imageName: "Mutualists (also anarcho-mututalists) .jpg"
+       },
+    {
+       r: 1,
+       x: -30,
+       y: -75,
+       z: -75,
+       url: "",
+       title: "Anarcha-feminists",
+       content: "Patriarchy dies with the state ",
+       imageName: "Anarcha-feminists.jpg"
+       },
+    {
+       r: 1,
+       x: -77,
+       y: -90,
+       z: 67,
+       url: "",
+       title: "Egoism",
+       content: "Everything I don't like is a spook. ",
+       imageName: "Egoism.png"
+       },
+    {
+       r: 1,
+       x: -85,
+       y: -90,
+       z: 99,
+       url: "",
+       title: "Anarcho-individualism",
+       content: "Revolution? More like self-evolution.",
+       imageName: "Anarcho-individualism.jpg"
+       },
+    {
+       r: 1,
+       x: -60,
+       y: 0,
+       z: 0,
+       url: "",
+       title: "Unconditional Accellerationism",
+       content: "The only way out is through",
+       imageName: "Unconditional Accellerationism.jpg"
+       },
+    {
+       r: 1,
+       x: -20,
+       y: 25,
+       z: -65,
+       url: "",
+       title: "Xenofemism/G-Acc",
+       content: "If nature is unjust, change nature.",
+       imageName: "XenofemismG-Acc.png"
+       },
+    {
+       r: 1,
+       x: -45,
+       y: 35,
+       z: -85,
+       url: "",
+       title: "Left Accelerationism",
+       content: "Fully Automated Luxury Communism",
+       imageName: "Left Accelerationism.jpg"
+       }
+    ]
